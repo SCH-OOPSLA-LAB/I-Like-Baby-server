3bc20a276bd7fafc84a8aab4090c559017c3a4ed
DongUk <az9170@naver.com>
2024-09-25 13:47:13 +0900

    feat: 프로젝트 생성

---
+++ ILikeBaby/src/main/java/oopsla/ILikeBaby/ILikeBabyApplication.java
@@ -0,0 +1,13 @@
+package oopsla.ILikeBaby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILikeBabyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ILikeBabyApplication.class, args);
+	}
+
+}
